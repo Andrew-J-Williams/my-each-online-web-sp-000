dd65e9c8fd1a9a65fb067f9c986ae1e548fe0113
Andrew Williams <1williamsaj@gmail.com>
2019-09-01 21:59:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
 def my_each(array) # put argument(s) here
-  if 
+  if block_given?
+    n = 0
+
+    while n < array.length
+      yield array[n]
+      n += 1
+    end
+
+    array
+  else
+    return nil
+  end
 end
