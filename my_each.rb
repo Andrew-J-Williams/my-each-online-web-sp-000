ea9c87163f5680760bf4cc1430f7b5094ff06857
Andrew Williams <1williamsaj@gmail.com>
2019-09-01 22:01:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
 def my_each(array) # Accepts the argument of the array.
   if block_given? # If a block is given for our method to take each value...
-    n = 0
+    n = 0 # ...then first, we create a variable n and set it to 0.
 
-    while n < array.length
-      yield array[n]
+    while n < array.length # Second, run a while loop that goes for the length of our array.
+      yield array[n] 
       n += 1
     end
 
