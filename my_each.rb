384208ae20463eebb59628c3b2bae94ea6f1d604
Andrew Williams <1williamsaj@gmail.com>
2019-09-01 22:03:50 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,11 +3,11 @@ def my_each(array) # Accepts the argument of the array.
     n = 0 # ...then first, we create a variable n and set it to 0.
 
     while n < array.length # Second, run a while loop that goes for the length of our array.
-      yield array[n] # Third, use 'yield' to 
-      n += 1
+      yield array[n] # Third, use 'yield' to pass the array information to the block when using a method.
+      n += 1 # Fourth, we increase the value of n by 1.
     end
 
-    array
+    array # Fifth, we return the value of the array
   else
     return nil
   end
